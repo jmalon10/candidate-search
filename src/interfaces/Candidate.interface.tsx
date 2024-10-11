878caa269db2c7094b39544291548912ac17abdc
Jackie Maloney <jmaloney11277@gmail.com>
2024-10-10 22:21:02 -0400

    adds candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,9 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly avatar_url: string | null;
+    readonly login: string| null;
+    readonly location: string| null;
+    readonly email: string| null;
+    readonly company: string| null;
+    readonly bio: string| null;
+}
